b999d793eb87f3ccd34d1d3b74576c7b61ccf563
tamar-mepe <tdavi18@freeuni.edu.ge>
2021-08-24 17:38:08 +0400

    add example for post request

---
+++ example5/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = new express();
+const path = require('path');
+
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.post('/action', function (request, response) {
+    response.send("Submitted!");
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, '127.0.0.1');
